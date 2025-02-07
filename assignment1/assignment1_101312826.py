46cba016484d0a53d2346d8c6b74da04d1193295
Avano <avanoberlingieri@gmail.com>
2025-02-06 22:49:37 -0500

    Almost done, just need to finish the last part

--- assignment1/assignment1_101312826.py
+++ assignment1/assignment1_101312826.py
@@ -2,31 +2,52 @@
 # Assignment 1
 
 gym_member = "Alex Alliton" # Data type: String
-preferred_weight_kg = 20.5 # Data type: Double
+preferred_weight_kg = 20.5 # Data type: Float
 highest_reps = 25 # Data type: Int
 membership_active = True # Data type: Boolean
 
 # Data types: String & tuple
 workout_stats = {
     "Nima": (0, 10, 90),
-    "Lhek": (15, 100, 60),
-    "Avano": (30, 60, 120),
-    "Nima_Total":(),
-    "Lhek_Total":(),
-    "Avano_Total":(),
+    "Lhek": (10, 60, 35),
+    "Avano": (30, 45, 75),
 }
 
-for i in workout_stats.keys():
-    workout_stats[f"{workout_stats.keys()} + _Total"] = (workout_stats[f"{workout_stats.keys()}"][0] + workout_stats[f"{workout_stats.keys()}"][1] + workout_stats[f"{workout_stats.keys()}"][2])
+totals = {}
+for name, minutes in workout_stats.items():
+    total_minutes = sum(minutes)
+    totals[f"{name}_total"] = total_minutes
 
-workout_List = ([],
-                [],
-                [])
+workout_stats.update(totals)
+print("Workout stats from dictionary:")
+print(workout_stats)
 
+# Data type: List of lists
+workout_list = [[workout_stats["Nima"][0], workout_stats["Nima"][1], workout_stats["Nima"][2]],
+                [workout_stats["Lhek"][0], workout_stats["Lhek"][1], workout_stats["Lhek"][2]],
+                [workout_stats["Avano"][0], workout_stats["Avano"][1], workout_stats["Avano"][2]]]
 
-if workout_stats[f"{workout_stats.keys()}_Total"] >= 120:
-    print(f"Great job staying active {workout_stats.keys()}!")
+print("Workout stats from nested lists:")
+print(workout_list)
 
-def find_friend():
-    for member in workout_stats.keys():
-        print(f"Member: {member} " + "yoga: " + workout_stats[member][0] + "running: " + workout_stats[member][1] + "Weightlifting: " + workout_stats[member][2] + "Total: " + workout_stats[member][3])
+yoga_running = [row[:2] for row in workout_list]
+print("Time for yoga & running:")
+print(yoga_running)
+
+weightlifting_last_two = [row[2] for row in workout_list[-2:]]
+print("Time weightlifting for Last Two Friends:")
+print(weightlifting_last_two)
+
+for name in workout_stats:
+    if "_total" in name and workout_stats[name] >= 120:
+        friend_name = name.replace("_total","")
+        print(f"Great job staying active {friend_name}!")
+
+friend = input("Enter a friend's name to check workout stats: ")
+
+if friend in workout_stats:
+    workout_data = workout_stats[friend]
+    print(f"{friend}'s Workout Stats - Yoga: {workout_data[0]}, Running: {workout_data[1]},"
+          f" Weightlifting: {workout_data[2]}, Total: {workout_stats[f'{friend}_total']}")
+else:
+    print(f"{friend} was not found in the records.")
