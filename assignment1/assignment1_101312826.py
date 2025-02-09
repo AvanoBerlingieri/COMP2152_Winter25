e12cb9b6a8e5c76ae3db290a3a047b063186a6fb
Avano <avanoberlingieri@gmail.com>
2025-02-09 18:39:38 -0500

    finished assignment 1

--- assignment1/assignment1_101312826.py
+++ assignment1/assignment1_101312826.py
@@ -6,7 +6,7 @@ preferred_weight_kg = 20.5 # Data type: Float
 highest_reps = 25 # Data type: Int
 membership_active = True # Data type: Boolean
 
-# Data types: String & tuple
+# Data type: dict
 workout_stats = {
     "Nima": (0, 10, 90),
     "Lhek": (10, 60, 35),
@@ -22,7 +22,7 @@ workout_stats.update(totals)
 print("Workout stats from dictionary:")
 print(workout_stats)
 
-# Data type: List of lists
+# Data type: List
 workout_list = [[workout_stats["Nima"][0], workout_stats["Nima"][1], workout_stats["Nima"][2]],
                 [workout_stats["Lhek"][0], workout_stats["Lhek"][1], workout_stats["Lhek"][2]],
                 [workout_stats["Avano"][0], workout_stats["Avano"][1], workout_stats["Avano"][2]]]
@@ -51,3 +51,9 @@ if friend in workout_stats:
           f" Weightlifting: {workout_data[2]}, Total: {workout_stats[f'{friend}_total']}")
 else:
     print(f"{friend} was not found in the records.")
+
+highest_friend = max(totals, key = totals.get)
+lowest_friend = min(totals, key = totals.get)
+
+print(f"{highest_friend.replace('_total', '')} spent the most time time working out: {totals[highest_friend]} minutes")
+print(f"{lowest_friend.replace('_total', '')} spent the least time working out: {totals[lowest_friend]} minutes")
